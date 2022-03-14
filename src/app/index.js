5ab1f3c4ab97aa4c4ac5a87875fe59235ba384d6
Poliak <poliak@spstrutnov.cz>
2022-03-14 09:36:34 +0100

    CSS main stranky

--- src/app/index.js
+++ src/app/index.js
@@ -6,5 +6,6 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use('/', require(path.join(__dirname, 'routers', 'router')));
-
+app.use(express.static(path.join(__dirname, 'www')));
+app.use(express.json());
 module.exports = app;
