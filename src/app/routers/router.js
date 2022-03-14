6fd0f7ada446d9aa96e30f7e628aed26751f4def
Martin Synek <synekm@spstrutnov.cz>
2022-03-14 09:09:49 +0100

    Oprava chyby v routeru + zacatek controlleru

--- src/app/routers/router.js
+++ src/app/routers/router.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
-const controller = require(path.join(__dirname, '..', controllers, 'controller'));
+const controller = require(path.join(__dirname, '..', 'controllers', 'controller'));
 
 router.get('/', controller.main);
 //router.get('/clanek/:id', controller.clanek);
