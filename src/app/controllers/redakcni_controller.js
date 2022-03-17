7b07712c9242546cb28bb982676f625feceddb0a
Poliak Tadeas <poliak@spstrutnov.cz>
2022-03-17 23:31:08 +0100

    CSS admina
    potreba dodelat odebrani clanku

--- src/app/controllers/redakcni_controller.js
+++ src/app/controllers/redakcni_controller.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const bcrypt = require("bcryptjs");
 const clanek_model = require(path.join(__dirname, '..', 'models', 'clanek_model'));
-const redakcniModel = require(path.join(__dirname, '..', 'models', 'redakcni_model'));
 
 
 exports.login_form = (req,res) =>
@@ -44,10 +43,11 @@ exports.post_login_info = (req, res,next) => {
 
 //Overeni zda je admin (redaktor) prihlaseny 
 exports.jePrihlasen = (req, res, next) => {
-    if (req.session.username == undefined) {
+    if (req.session.userid == undefined)
+     {
         res.redirect("/redakce/login");
     }
-    if (req.session.username == 'admin') 
+    if (req.session.userid == "admin") 
     {
         next();
     }
@@ -57,18 +57,20 @@ exports.odhlasit = (req, res) =>
 {
     req.session.username = undefined;
     req.session.password = undefined;
+    req.session.userid = undefined;
     res.send({"msg":{"status":100, "text":"Success"}})
 }
 
 exports.porovnat = (req, res) => {
     bcrypt.hash(process.env.ADMIN_PASSWORD, 5, function (err, hash) {
         bcrypt.compare(req.session.password, hash, function (err, result) {
-            if(req.session.username == process.env.ADMIN_USERNAME && result == true){
+            if(req.session.username == process.env.ADMIN_USERNAME && result == true)
+            {
                 req.session.userid = 'admin';
                 res.redirect('/redakce/nahrani');
             }
             else {
-                res.redirect('/redakce/login')
+                res.redirect('/redakce/login');
             }
         });
     }); 
